3fe2eb85dbba650039f4c4bdc221e1c2ef4a6bc7
Mri-cia <rawikara8c@gmail.com>
2025-11-27 07:31:42 +0700

    adt

---
+++ node.cpp
@@ -0,0 +1,13 @@
+#include "node.h"
+
+using namespace std;
+
+adrNode createElm(infotype x){
+    adrNode p = new Bishops;
+    p->info = x;
+    p->children = nullptr;
+    return p;
+};
+void createTree(Node &N){
+   N->info =
+};
--- node.h
+++ node.h
@@ -1,20 +1,34 @@
 #ifndef NODE_H_INCLUDED
 #define NODE_H_INCLUDED
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 typedef struct Node *adrNode;
+typedef struct Bishops infotype;
 
 struct Bishops{
     string name;
     int reign;
     string origin;
-}
+};
 
 struct Node{
-    Bishops info;
-    adrNode left;
-    adrNode right;
+    infotype info;
+
+    vector<adrNode> children;
 };
+
+
+adrNode createElm(infotype x);
+void createTree(adrNode &p);
+void insertNode(adrNode &p, adrNode q);
+void deleteNode(adrNode &, adrNode q);
+adrNode searchNode(adrNode p);
+void printNode(adrNode p);
+void showPostTraversal(adrNode p);
+void showPreTraversal(adrNode p);
+void showInOrderTraversal(adrNode p);
 
 #endif // NODE_H_INCLUDED
